312f44e2f5c6acea52f44671a987176f1609c772
MohsenAzimi75 <mohsenazimi7525@gmail.com>
2024-02-14 12:00:59 +0330

    3 Commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Posts from "./posts/page";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Posts />
+    </main>
+  );
+}
